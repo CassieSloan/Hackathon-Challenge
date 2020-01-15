1cdc824786a4c1ce7bd10eee691e583910b31672
Cassie <cassandra.sloan16@gmail.com>
2020-01-15 15:25:58 +1100

    additional styling

--- src/components/breathing/BreathingComponent.js
+++ src/components/breathing/BreathingComponent.js
@@ -6,16 +6,16 @@ import { BrowserRouter, Link } from "react-router-dom";
 class BreathingComponent extends Component {
     render() {
         return (
-            <>
-                <h1>Welcome to your breathing aid</h1>
-                <h3>Pick a breathing method</h3>
+            <div className="container">
+                <h1 className="text">Welcome to your breathing aid</h1>
+                <h3 className="text">Pick a breathing method</h3>
                 <BrowserRouter>
-                    <Link to="/breathing/four-four">Four fours</Link>
-                    <Link to="/breathing/four-seven">Four seven</Link>
-                    <Link to="/breathing/seven-eleven">Seven eleven</Link>
+                    <Link className="text" to="/breathing/four-four">Four fours</Link>
+                    <Link className="text" to="/breathing/four-seven">Four seven</Link>
+                    <Link className="text" to="/breathing/seven-eleven">Seven eleven</Link>
                 </BrowserRouter>
 
-            </>
+            </div>
         )
     }
 }
