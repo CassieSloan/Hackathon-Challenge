880fa4ebf0db8f180e9b9043c5cfa9631dc5a59a
chelseal <chelseablove@gmail.com>
2020-01-15 15:31:44 +1100

    Committing features

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
-
+import Unsplash from "./Unsplash";
 
 class App extends Component {
     render() {
         return (
+        <>
             <h1>This is our hackathon app!</h1>
+            <Unsplash />
+        </>
         )
     }
-} 
+}
 
 export default App;
