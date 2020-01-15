fdeecb18967a4592bb5aaba62463f09e8ff3d9b4
poklipart <angellopezandrade4@gmail.com>
2020-01-15 15:33:18 +1100

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from "react";
-
+import { BrowserRouter, Route } from "react-router-dom";
+import WorldFactsForm from "./WorldFactsForm";
 
 class App extends Component {
-    render() {
+    render () {
         return (
-            <h1>This is our hackathon app!</h1>
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Route exact path="/world_facts_form" component={WorldFactsForm} />
+                    </div>
+                </BrowserRouter>
+            </div>
         )
     }
-} 
+}
 
 export default App;
