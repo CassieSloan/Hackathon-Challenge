14a9434012d5c549c78e79532725ef8273cc31ff
chelseal <chelseablove@gmail.com>
2020-01-15 16:11:53 +1100

    done

--- src/components/Unsplash.js
+++ src/components/Unsplash.js
@@ -2,39 +2,47 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class Unsplash extends Component {
+    state = {imageURL: ""}
     render() {
 
+
         const fetchWater = async () => {
+            const random = [1,2,3,4,5,6,7,8,9,10]
+            const randomNumber = random[Math.floor(Math.random()*random.length)];
             await axios.get("https://api.unsplash.com/search/photos", {
             params: { query: "water" },
             headers: {
                 Authorization: 'Client-ID 1e879569e8ae76d56bed858949e9c98a062760c982e7cef677e3895a784d8a28'
             }
-        }).then((image) => console.log(image.data.results))
-            
-            console.log("this is the fetchWaterfunction");
+        }).then((image) => {
+            this.setState({imageURL: image.data.results[randomNumber].urls.regular} )
+            })
         }
 
         const fetchNature = async () => {
+            const random = [1,2,3,4,5,6,7,8,9,10]
+            const randomNumber = random[Math.floor(Math.random()*random.length)];
             await axios.get("https://api.unsplash.com/search/photos", {
             params: { query: "nature" },
             headers: {
                 Authorization: 'Client-ID 1e879569e8ae76d56bed858949e9c98a062760c982e7cef677e3895a784d8a28'
             }
-        }).then((image) => console.log(image.data.results))
-            
-            console.log("this is the fetchNature function");
+        }).then((image) => {
+            this.setState({imageURL: image.data.results[randomNumber].urls.regular} )
+            })
         }
 
         const fetchAnimal = async () => {
+            const random = [1,2,3,4,5,6,7,8,9,10]
+            const randomNumber = random[Math.floor(Math.random()*random.length)];
             await axios.get("https://api.unsplash.com/search/photos", {
             params: { query: "animals" },
             headers: {
                 Authorization: 'Client-ID 1e879569e8ae76d56bed858949e9c98a062760c982e7cef677e3895a784d8a28'
             }
-        }).then((image) => console.log(image.data.results))
-            
-            console.log("this is the fetchAnimal function");
+        }).then((image) => {
+            this.setState({imageURL: image.data.results[randomNumber].urls.regular} )
+            })
         }
 
         return (
@@ -45,7 +53,7 @@ class Unsplash extends Component {
                     <button onClick={fetchWater} type="button">Water</button>
                     <button onClick={fetchNature} type="button">Nature</button>
                     <button onClick={fetchAnimal} type="button">Animal</button>
-                        <img src="examplephoto.jpg" alt="Tree" width="300" height="200"></img>
+                        <img src={this.state.imageURL} alt="Tree" width="300" height="200"></img>
             </div>
             </>
         )
